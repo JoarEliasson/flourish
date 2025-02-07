14e942108090a5b104cc6769fa515eeef4afe457
Joar Eliasson <joar.eliasson@gmail.com>
2025-02-07 13:14:47 +0100

    Fixed imports
    Signed-off-by: Joar Eliasson <joar.eliasson@gmail.com>

--- src/main/java/se/myhappyplants/api/TrefleApiClient.java
+++ src/main/java/se/myhappyplants/api/TrefleApiClient.java
@@ -10,9 +10,7 @@ import se.myhappyplants.shared.PlantDetails;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.http.*;
 import java.util.ArrayList;
 import java.util.List;
 
