b5d554963dd28e13bbdd34ffd0ee2aa938a99dea
utbkenalt <utbkenalt@gmail.com>
2025-02-15 13:42:58 +0100

    Create PasswordTest.java

---
+++ flourish-client/src/test/java/PasswordTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordTest {
+
+    @Test
+    public void testDerov_withValidString() { //And lower case
+    }
+}
