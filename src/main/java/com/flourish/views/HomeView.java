920acd9f5d8f52b5b5ebff08cfe2f5cf3ada49b0
emilaqvist <emilaqvist@gmail.com>
2025-02-28 14:12:50 +0100

    HomeView Finish (Test picture added, not supposed to be in prod!)

--- src/main/java/com/flourish/views/HomeView.java
+++ src/main/java/com/flourish/views/HomeView.java
@@ -1,9 +1,12 @@
 package com.flourish.views;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -24,14 +27,26 @@ public class HomeView extends VerticalLayout {
         Paragraph desc = new Paragraph("Your personal plant library!");
         desc.addClassNames(LumoUtility.TextColor.SECONDARY,LumoUtility.FontSize.LARGE);
 
-        Image plantImage = new Image("images/PlantBild.jpg", "Plant Here!");
+        Button myPlantsNavigation = new Button("My Plants!", e -> getUI().ifPresent(ui -> ui.navigate("my-plants")));
+        Button allPlantsNavigation = new Button("All Plants!", e -> getUI().ifPresent(ui -> ui.navigate("all-plants")));
+
+        myPlantsNavigation.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        allPlantsNavigation.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(myPlantsNavigation,allPlantsNavigation);
+        buttonLayout.setPadding(true);
+        buttonLayout.setSpacing(true);
+
+        Image plantImage = new Image("https://plus.unsplash.com/premium_photo-1675864663002-c330710c6ba0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGxhbnR8ZW58MHx8MHx8fDA%3D", "Plant Here!");
+
         plantImage.setWidth("200px");
         plantImage.getStyle().set("border-radius","15px").set("box-shadow","0px 4px 10px rgba(0,0,0,0.1)");
+
 
 
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
         setSizeFull();
-        add(title,desc,plantImage);
+        add(title,desc,buttonLayout,plantImage);
     }
 }
